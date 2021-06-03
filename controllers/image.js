aeadfee0e30dc163267141dac3ae676af0c17649
Kevin Clark <Clark.KevinLouis@gmail.com>
2021-06-03 13:43:00 -0700

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+    apiKey: 'a14bfacfea884dde9e36dd9852899eee'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            response.json(data)
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+    handleImage, handleApiCall
+}
